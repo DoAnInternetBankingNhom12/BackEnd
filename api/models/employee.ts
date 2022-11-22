03bdb1a63e4d9eef59491a8a69a8bd1e3929124b
T.R.U.C <xsnowflakes1x@gmail.com>
2022-11-22 14:56:05 +0700

    fix: type idCustomer to idUser in model Employee

--- api/models/employee.ts
+++ api/models/employee.ts
@@ -2,7 +2,7 @@ import { Schema, Document, model } from "mongoose";
 
 const employeeSchema = new Schema<Employee>({
     id: { type: String, required: true, unique: true },
-    idCustomer: { type: String, required: true },
+    idUser: { type: String, required: true },
     name: {type: String, required: true },
     phoneNumbers: { type: [String], required: true },
     accountType: { type: String, required: true },
@@ -13,7 +13,7 @@ const employeeSchema = new Schema<Employee>({
 
 export interface Employee extends Document {
     id: string,
-    idCustomer: string,
+    idUser: string,
     name: string,
     phoneNumbers: string[],
     accountType: string,

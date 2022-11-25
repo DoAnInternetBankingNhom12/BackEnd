bc444e3778ae09792ca06d0c553358135c425885
T.R.U.C <xsnowflakes1x@gmail.com>
2022-11-25 13:37:07 +0700

    remove: old funtion get transaction by user id

--- api/controllers/transaction.controller.ts
+++ api/controllers/transaction.controller.ts
@@ -16,37 +16,6 @@ class TransactionCtrl extends BaseCtrl {
   table = 'Transaction';
 
   // Find
-  getByUserId = async (req: Request, res: Response) => {
-    try {
-      const optionSearch = lodash.cloneDeep(req.query);
-      console.log(optionSearch);
-      const obj = await this.model.find(optionSearch, { _id: 0, __v: 0, _status: 0 });
-
-      if (isNull(obj)) {
-        return res.status(400).json({
-          mgs: `Get data ${this.table} not exist!`,
-          success: false
-        });
-      }
-
-      return res.status(200).json({
-        data: obj,
-        success: true
-      });
-    } catch (err: any) {
-      return res.status(400).json({
-        mgs: `Get ${this.table} id ${req.body.id} error!`,
-        data: req.params.id,
-        success: false,
-        error: {
-          mgs: err.message,
-          status: 400,
-          code: 5000
-        }
-      });
-    }
-  };
-
   findTransaction = async (req: Request, res: Response) => {
     try {
       const optionSearch = lodash.cloneDeep(req.query);

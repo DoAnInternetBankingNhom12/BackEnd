384399adb32b41792240fe27423cdd8e74a5fa6f
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-01 18:03:44 +0700

    update: function sendMail return Promise

--- api/services/mail.service.ts
+++ api/services/mail.service.ts
@@ -11,22 +11,27 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-const sentMail = (user: User, code: string) => {
-  const mailOptions = {
-    from: process.env.EMAIL,
-    to: user.email,
-    subject: 'Email OTP mã xác thực FTB bank',
-    html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mà xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${code}</h1><p>Mã xác thực này chủ có hiệu lực trong vòng 5 phút!</p>`
-  };
+export function sendMail(user: User, OTP: string) {
+  return new Promise((resolve) => {
+    const mailOptions = {
+      from: process.env.EMAIL,
+      to: user.email,
+      subject: 'Email OTP mã xác thực FTB bank',
+      html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mà xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${OTP}</h1><p>Mã xác thực này chủ có hiệu lực trong vòng 5 phút!</p>`
+    };
 
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        resolve({
+          success: false,
+          err: error
+        });
+      } else {
+        resolve({
+          success: true,
+          mgs: `Email sent: ${info.response}`
+        });
+      }
+    });
   });
-
 }
-
-export default sentMail;

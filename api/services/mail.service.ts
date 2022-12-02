08d18add9638e5a9eaa50e2da607c351ee686b51
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-02 13:22:38 +0700

    update: messsage sent in email mail service

--- api/services/mail.service.ts
+++ api/services/mail.service.ts
@@ -17,7 +17,7 @@ export function sendMail(user: User, OTP: string) {
       from: process.env.EMAIL,
       to: user.email,
       subject: 'Email OTP mã xác thực FTB bank',
-      html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mà xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${OTP}</h1><p>Mã xác thực này chủ có hiệu lực trong vòng 5 phút!</p>`
+      html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mà xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${OTP}</h1><p>Mã xác thực này chỉ có hiệu lực trong vòng <b>5 phút</b>!</p>`
     };
 
     transporter.sendMail(mailOptions, function (error, info) {

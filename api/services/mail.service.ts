2f3d49fd9eb3357a2ad4c36f27a05b2bc0f6524f
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-04 11:11:23 +0700

    update: html sendMail

--- api/services/mail.service.ts
+++ api/services/mail.service.ts
@@ -17,7 +17,7 @@ export function sendMail(user: User, OTP: string) {
       from: process.env.EMAIL,
       to: user.email,
       subject: 'Email OTP mã xác thực FTB BANK',
-      html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mã xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${OTP}</h1><p>Mã xác thực này chỉ có hiệu lực trong vòng <b>5 phút</b>!</p>`
+      html: `<p>Thân gửi tài khoản <b>${user.userName}</b>!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mã xác thực của chúng tôi. Mã xác thực là:\n</p><h1>${OTP}</h1><p>Mã xác thực này chỉ có hiệu lực trong vòng <b>5 phút</b>!</p>`
     };
 
     transporter.sendMail(mailOptions, function (error, info) {
@@ -43,7 +43,7 @@ export function sendPassMail(user: User, pass: string) {
       from: process.env.EMAIL,
       to: user.email,
       subject: 'Email mã đăng nhập FTB BANK',
-      html: `<p>Thân gửi tài khoản ${user.userName}!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mã đăng nhập của chúng tôi. Mã đăng nhập là:\n</p><h1>${pass}</h1><p>Yêu cầu tài khoản ${user.userName} đổi mật khẩu sau lần đăng nhập đầu tiên!</p>\n<p>Nếu có vấn đề gì liên quan đến mặt khẩu mặc định không được đổi chúng tôi sẽ không chịu trách nhiệm!</p>\n<h3>Xin cảm ơn!</h3>\n<h2>FTP BANK</h2>`
+      html: `<p>Thân gửi tài khoản <b>${user.userName}</b>!\n</p><h2>Chúng tôi là FTB BANK</h2><p>Bạn đã chọn email ${user.email} để nhận mã đăng nhập của chúng tôi. Mã đăng nhập là:\n</p><h1>${pass}</h1><p>Yêu cầu tài khoản ${user.userName} đổi mật khẩu sau lần đăng nhập đầu tiên!</p>\n<p>Nếu có vấn đề gì liên quan đến mặt khẩu mặc định không được đổi chúng tôi sẽ không chịu trách nhiệm!</p>\n<h3>Xin cảm ơn!</h3>\n<h2>FTP BANK</h2>`
     };
 
     transporter.sendMail(mailOptions, function (error, info) {

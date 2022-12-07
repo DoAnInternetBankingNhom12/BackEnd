bdbdf68a2c62c2f5c34b8299967530b4f163002b
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-07 14:27:58 +0700

    update: new bank model

--- api/models/bank.ts
+++ api/models/bank.ts
@@ -3,16 +3,18 @@ import { Schema, Document, model } from 'mongoose';
 const bankSchema = new Schema<Bank>({
   id: { type: String, required: true, unique: true },
   name: { type: String, required: true, unique: true },
-  phoneNumbers: { type: [String]},
-  addresses: { type: [String]},
+  type: { type: String, required: true },
+  phoneNumbers: { type: [String] },
+  addresses: { type: [String] },
   createTime: Number,
   updateTime: Number,
-  _status: { type: Boolean, required: true}
+  _status: { type: Boolean, required: true }
 }, { collection: 'bank' });
 
 export interface Bank extends Document {
   id: string,
   name: string,
+  type: string, // internal: Ngân hàng nội bộ, external: Ngân hàng liên kết.
   phoneNumbers: string[],
   addresses: string[],
   createTime: number,

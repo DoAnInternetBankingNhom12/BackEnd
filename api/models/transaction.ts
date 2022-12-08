7320464966fe2e53dd4a48641e48e5c7d254a002
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-08 11:45:56 +0700

    update: transaction model

--- api/models/transaction.ts
+++ api/models/transaction.ts
@@ -2,13 +2,16 @@ import { Schema, Document, model } from 'mongoose';
 
 const transactionSchema = new Schema<Transaction>({
   id: { type: String, required: true, unique: true },
-  userId: { type: String, required: true },
-  receiverId: { type: String, required: true },
-  receivingBankId: { type: String, required: true },
+  sendPayAccount: { type: String, required: true },
+  sendBankId: { type: String, required: true },
+  receiverPayAccount: { type: String, required: true },
+  receiverBankId: { type: String, required: true },
+  payAccountFee: { type: String, required: true },
+  transactionFee: { type: Number, required: true },
   amountOwed: { type: Number, required: true },
-  transactionFee: { type: Number },
   description: { type: String },
-  status: { type: String, required: true},
+  statusTransaction: { type: String, required: true},
+  statusMoney: { type: String, required: true},
   typeTransaction: { type: String, required: true},
   createTime: Number,
   updateTime: Number,
@@ -17,13 +20,16 @@ const transactionSchema = new Schema<Transaction>({
 
 export interface Transaction extends Document {
   id: string,
-  userId: string,
-  receiverId: string,
-  receivingBankId: string,
-  amountOwed: number,
+  sendPayAccount: string,
+  sendBankId: string,
+  receiverPayAccount: string,
+  receiverBankId: string,
+  payAccountFee: string,
   transactionFee: number,
+  amountOwed: number,
   description: string,
-  status: string, // transferring: Đang chuyển tiền, completed: Chuyển tiền thành công, failed: Chuyển thất bại, not_yet_delivered: Chưa giao tiền, delivered: Đã giao tiền
+  statusTransaction: string, // transferring: Đang chuyển tiền, completed: Chuyển tiền thành công, failed: Chuyển thất bại.
+  statusMoney: string, // not_delivered: Chưa giao tiền, delivered: Đã giao tiền
   typeTransaction: string // internal: Trong ngân hàng, external: Ngoài ngân hàng
   createTime: number,
   updateTime: number,

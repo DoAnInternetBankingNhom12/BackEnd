6fc7f3e80df8aa28b26068edb8a5eef7a2e91a20
T.R.U.C <xsnowflakes1x@gmail.com>
2022-12-09 12:38:41 +0700

    update: getCustomerByPayNumber in custommer controller

--- api/controllers/customer.controller.ts
+++ api/controllers/customer.controller.ts
@@ -10,6 +10,7 @@ import Employee from '../models/employee';
 
 // Utils
 import * as moment from 'moment';
+import * as lodash from 'lodash';
 import { isNull } from '../utils/utils';
 
 class CustomerCtrl extends BaseCtrl {
@@ -17,6 +18,41 @@ class CustomerCtrl extends BaseCtrl {
   modelUser = User;
   modelEmployee = Employee;
   table = 'Customer';
+
+  // Get
+  
+  getCustomerByPayNumber = async (req: Request, res: Response) => {
+    try {
+      const paymentAccount = lodash.cloneDeep(req.params.paymentAccount);
+
+      if (isNull(paymentAccount)) {
+        return res.status(400).json({
+          mgs: `No payment account to get!`,
+          success: false
+        });
+      }
+
+      const account = await this.model.findOne({paymentAccount: paymentAccount, _status: true }, { id:0, _id: 0, __v: 0, _status: 0, userId: 0, accountBalance: 0, createTime: 0, updateTime: 0});
+      
+      if (isNull(account)) {
+        return res.status(400).json({
+          mgs: 'Payment account not exist!',
+          success: false
+        });
+      }
+
+      return res.status(200).json({
+        data: account,
+        success: true
+      });
+    } catch (err: any) {
+      return res.status(400).json({
+        mgs: `Get account ${this.table} error!`,
+        success: false,
+        error: err
+      });
+    }
+  }
 
   // Create
   createCustomer = async (req: Request, res: Response) => {

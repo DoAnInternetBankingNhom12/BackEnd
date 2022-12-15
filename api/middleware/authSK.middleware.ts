78bfeab31ff01b3ef3ce4d6cf52de4a2e8b341df
Rokia7 <hahoanglong7@gmail.com>
2022-12-15 12:22:47 +0700

    update: get info bank in authSK middleware

--- api/middleware/authSK.middleware.ts
+++ api/middleware/authSK.middleware.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 
+// Models
+import Bank from '../models/bank';
+
 // Utils
 import { isNull, decryptedData } from '../utils/utils';
 import * as crypto from 'crypto';
@@ -7,10 +10,21 @@ import * as moment from 'moment';
 import * as lodash from 'lodash';
 
 const verifySK = async (req: Request, res: Response, next: any) => {
+  const model = Bank;
   const tokenSK: string = req.headers['tokensk'] as string;
   const objEncode: any = decryptedData(tokenSK);
   const token = req.headers['token'];
   const time = objEncode.time;
+  const bankInfo: any = await model.findOne({ id: objEncode.bankId, _status: true }, { _id: 0, __v: 0, _status: 0 });
+
+  if (isNull(bankInfo)) {
+    return res.status(401).json({
+      status: false,
+      errors: {
+        mgs: "No data bank or no bankId field!"
+      }
+    });
+  }
 
   if (!time) {
     return res.status(401).json({
@@ -44,6 +58,7 @@ const verifySK = async (req: Request, res: Response, next: any) => {
   const url = lodash.cloneDeep(req.originalUrl);
   const hmac = crypto.createHmac('sha256', key);
   const hashCreateToken = hmac.update(`${key}${url}${time}`).digest('hex');
+  req.body.bankInfo = bankInfo;
 
   if (hashCreateToken !== token) {
     return res.status(401).json({
